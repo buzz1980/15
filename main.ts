0750e57480f6ea64bac5895dd4fdb1fbb40def1b
buzz1980 <105562851+buzz1980@users.noreply.github.com>
2022-05-29 07:49:50 +0200

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,29 @@
+let wapen = 0
+input.onGesture(Gesture.Shake, function () {
+    wapen = randint(0, 2)
+    if (wapen == 0) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    } else if (wapen == 1) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    }
+})
